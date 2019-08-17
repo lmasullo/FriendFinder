bba8454299b4505b13cc22596083f3677f9867fd
Larry Masullo <lmasullo@gmail.com>
2019-08-17 13:36:42 -0500

    route

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 // Dependencies
-// ===========================================================
+// =============================================================
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Route to home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'home.html'));
+});
 
 // Listener
 // ===========================================================
