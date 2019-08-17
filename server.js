557c087820bd2b87114622ee5a3307f66fc37c97
Larry Masullo <lmasullo@gmail.com>
2019-08-17 13:42:03 -0500

    survey

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/app/public/home.html'));
 });
 
+// Route to Survey page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/app/public/survey.html'));
+});
+
 // Listener
 // ===========================================================
 app.listen(PORT, () => {
