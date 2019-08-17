f1ee6a3a5e2cc278f41311bd069f21e29c5c6850
Larry Masullo <lmasullo@gmail.com>
2019-08-17 13:43:10 -0500

    survey

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 });
 
 // Route to Survey page
-app.get('/', (req, res) => {
+app.get('/survey', (req, res) => {
   res.sendFile(path.join(__dirname, '/app/public/survey.html'));
 });
 
