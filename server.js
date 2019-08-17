4c364d73085b8eb5f72a33859d263405d446783c
Larry Masullo <lmasullo@gmail.com>
2019-08-17 13:29:30 -0500

    setup

---
+++ server.js
@@ -0,0 +1,12 @@
+// Dependencies
+// ===========================================================
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+// Listener
+// ===========================================================
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
