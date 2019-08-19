57eb93e78deda5a950208862b455e313156a924b
Lawrence N. Masullo <lmasullo@gmail.com>
2019-08-18 20:15:47 -0500

    changed image address

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,17 +2,20 @@
 const friends = [
   {
     name: 'Jose',
-    photo: 'https://pixabay.com/photos/human-man-face-view-bart-hat-1411499/',
+    photo:
+      'https://cdn.pixabay.com/photo/2016/05/23/23/32/human-1411499_960_720.jpg',
     scores: ['5', '5', '5', '5', '5', '5', '4', '3', '2', '1'],
   },
   {
     name: 'Dirk',
-    photo: 'https://pixabay.com/photos/man-face-wet-male-head-person-945482/',
+    photo:
+      'https://cdn.pixabay.com/photo/2015/09/18/11/46/man-945482_960_720.jpg',
     scores: ['2', '2', '3', '1', '5', '4', '2', '1', '4', '5'],
   },
   {
     name: 'Joy',
-    photo: 'https://pixabay.com/photos/girl-beauty-woman-model-hair-2359121/',
+    photo:
+      'https://cdn.pixabay.com/photo/2017/05/31/04/59/girl-2359121_960_720.jpg',
     scores: ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
   },
 ];
